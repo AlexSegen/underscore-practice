a65d5d59e3bb13e5b1ecdfaf26c52632e278f3a8
Alejandro Vivas <alejandrojv17@gmail.com>
2018-08-30 16:37:19 -0300

    Find First

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -53,93 +53,23 @@ $.fn.sortBy = function(arg, active) {
   $.fn.printList(x);
 };
 
-$.fn.Details = id => {
-  //$.getJSON(apiURL + '/' + id, (data) => {
+$.fn.findFirst = function(id) {
+  var data = $.fn.loadData();
 
-  $.getJSON(apiURL + "/" + id, data => {})
-    .then(res => {
-      json = res;
+  var x = _.find(data, function(find) {
+    return find.id == id;
+  });
 
-      modalContent.empty();
-      modalFooter.empty();
-      modalContent.append(`
-            <div class="panel panel-default">
-            <div class="panel-heading">
-            <h4 class="panel-title">User profile</h4>
-            </div>
-            <div class="panel-body">
-              <div class="profile__avatar">
-                <img src="https://api.adorable.io/avatars/285/${
-                  json.email
-                }.png" alt="...">
-              </div>
-              <div class="profile__header">
-                <h4>${json.name} <small>${json.username}</small></h4>
-                <p class="text-muted">
-                ${json.email}
-                </p>
-                <p>
-                  <a href="http://${json.website}/" target="_blank">${
-        json.website
-      }</a>
-                </p>
-              </div>
-            </div>
-          </div>
-  
-          <div class="panel panel-default">
-            <div class="panel-heading">
-            <h4 class="panel-title">User info</h4>
-            </div>
-            <div class="panel-body">
-              <table class="table profile__table">
-                <tbody>
-                  <tr>
-                    <th><strong>Location</strong></th>
-                    <td>${json.address.city}, ${json.address.street} ${
-        json.address.suite
-      }</td>
-                  </tr>
-                  <tr>
-                    <th><strong>Phone</strong></th>
-                    <td>${json.phone}</td>
-                  </tr>
-                  <tr>
-                    <th><strong>Company name</strong></th>
-                    <td>${json.company.name}</td>
-                  </tr>
-                  <tr>
-                    <th><strong>Description</strong></th>
-                    <td>${json.company.catchPhrase}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>`);
-      modalFooter.append(`
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-danger --btn-delete" data-id="${
-              json.id
-            }">Delete</button>
-          `);
-    })
-    .catch(err => {
-      modalContent.empty();
-      modalContent.append(`
-            <li class='media'>
-            <div class='alert alert-danger'>Error while fetching data.</div>
-            </li>`);
-    });
+  $.fn.printDetails(x);
 };
 
-$.fn.printDetails = function() {
-  var data = $.fn.loadData();
+$.fn.printDetails = function(value) {
+  //var data = $.fn.loadData();
 
   modalContent.empty();
   modalFooter.empty();
 
-  $.each(data, function(index, value) {
-    modalContent.append(`
+  modalContent.append(`
         <div class="panel panel-default">
         <div class="panel-heading">
         <h4 class="panel-title">User profile</h4>
@@ -156,9 +86,9 @@ $.fn.printDetails = function() {
             ${value.email}
             </p>
             <p>
-              <a href="http://${
-                value.website
-              }/" target="_blank">${value.website}</a>
+              <a href="http://${value.website}/" target="_blank">${
+    value.website
+  }</a>
             </p>
           </div>
         </div>
@@ -173,9 +103,9 @@ $.fn.printDetails = function() {
             <tbody>
               <tr>
                 <th><strong>Location</strong></th>
-                <td>${
-                  value.address.city
-                }, ${value.address.street} ${value.address.suite}</td>
+                <td>${value.address.city}, ${value.address.street} ${
+    value.address.suite
+  }</td>
               </tr>
               <tr>
                 <th><strong>Phone</strong></th>
@@ -193,13 +123,15 @@ $.fn.printDetails = function() {
           </table>
         </div>
       </div>`);
-    modalFooter.append(`
+  modalFooter.append(`
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
         <button type="button" class="btn btn-danger --btn-delete" data-id="${
           value.id
         }">Delete</button>
       `);
-  });
+  //});
+
+  $("#myModal").modal("show");
 };
 
 $.fn.printList = function(data) {
@@ -224,7 +156,7 @@ $.fn.printList = function(data) {
                  <div class="mt-20">
                     <button type="buton" class="btn btn-primary btn-xs --see-details" data-id="${
                       value.id
-                    }" data-toggle="modal" data-target="#myModal">Details</button>
+                    }">Details</button>
                  </div>
                </div>
          
@@ -269,36 +201,21 @@ $(document).on("click", ".btnfetchData", function() {
 
 $(document).on("click", ".--see-details", function() {
   let id = $(this)[0].dataset.id;
-
-  //$.fn.Details(id);
-
-  $.fn.printDetails(id);
+  $.fn.findFirst(id);
 });
 
 $(document).on("click", ".--btn-delete", function() {
-  let id = $(this)[0].dataset.id;
+  let id = $(this).data("id");
 
   if (confirm("Are you sure?")) {
     $.fn.Delete(id);
   }
 });
 
-/*    $(document).on('click','.btnsortEmail',function(){
-
-        let $this = $(this)[0].children[0];
-
-        let data = _.sortBy($.fn.fetchData($this), function(sort){ 
-            return sort.email; 
-        });
-
-        $.fn.printList(data);
-
-    }); */
-
 $(document).ready(function() {
   $(document).on("click", ".--sortBy", function() {
-    var sorting = $(this)[0].dataset.sort;
-    var $this = $(this)[0].dataset.active;
+    var sorting = $(this).data("sort");
+    var $this = $(this).data("active");
 
     var asc = "ASC";
     var desc = "DESC";
